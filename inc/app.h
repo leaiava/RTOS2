c80b1cca9524f34a5e4d2612ff2c898ea625b881
Leandro Arrieta <leandroarrieta@gmail.com>
2021-11-24 16:59:49 -0300

    Se implementa C3 en archivo app.c y app.h

--- inc/app.h
+++ inc/app.h
@@ -10,19 +10,32 @@
 
 #include "separacion_frames.h"
 
-#define CANT_PALABRAS_MIN   1
-#define CANT_PALABRAS_MAX   15
-#define CANT_LETRAS_MIN     1
-#define CANT_LETRAS_MAX     10
+#define CANT_PALABRAS_MIN       1   // R_C3_2
+#define CANT_PALABRAS_MAX       15  // R_C3_1
+#define CANT_LETRAS_MIN         1   // R_C3_4
+#define CANT_LETRAS_MAX         10  // R_C3_3
 
+#define SIN_ERROR               -1
+#define ERROR_INVALID_DATA      0
+#define ERROR_INVALID_OPCODE    1
+#define ERROR_SYSTEM            2
+
+#define CARACTER_INICIAL        0
+#define PALABRA_INICIAL         0
+
+#define INDICE_CAMPO_DATOS      1
+#define INDICE_CAMPO_C          0
+
+#define A_MINUSCULA             32  // 32 es la diferencia entre un caracter en mayúscula y uno en minúscula.
+#define A_MAYUSCULA             -32
 void task_app(void* pvParameters);
 
 typedef struct 
 {
-    uint8_t palabras[CANT_LETRAS_MAX][CANT_PALABRAS_MAX];  ///> Array de strings para extraer las palabras del mensaje
+    uint8_t palabras[CANT_PALABRAS_MAX][CANT_LETRAS_MAX];  ///> Array de strings para extraer las palabras del mensaje
     uint8_t error_type;                                    ///> Variable para guardar el tipo de error en el mensaje
     tMensaje mensaje;                                      ///> Variable para recibir el mensaje
 } app_t;
 
 app_t* app_crear(void);
-bool app_init(app_t* handler);
+void app_init(app_t* handler);
--- src/app.c
+++ src/app.c
@@ -10,9 +10,8 @@
 
 #include "app.h"
 
-static void app_extraer_palabras(app_t* handler_app);
-static bool app_validar_mensaje(app_t* handler_app);    // TODO: Ver si conviene pasarle la estructura o solo un char.
-static void app_procesar_mensaje(app_t* handler_app);
+static bool app_extraer_palabras(app_t* handler_app);
+static bool app_procesar_mensaje(app_t* handler_app);
 static void app_insertar_mensaje_error(app_t* handler_app);  
 
 void task_app(void* pvParameters)
@@ -25,15 +24,15 @@ void task_app(void* pvParameters)
 
 	while(TRUE)
 	{
-		if(!sf_mensaje_recibir(handler, &ptr_app->mensaje ))
-			error_handler();
+		sf_mensaje_recibir(handler, &ptr_app->mensaje );
+		
+        if (app_extraer_palabras(ptr_app) == 0)
+            app_insertar_mensaje_error(ptr_app);    // Si salió con error, inserto el mensaje de error para ser enviado
         
-        app_extraer_palabras(ptr_app);
+        else if (app_procesar_mensaje(ptr_app) == 0)
+            app_insertar_mensaje_error(ptr_app);    // Si salió con error, inserto el mensaje de error para ser enviado
 
-        app_validar_mensaje(ptr_app);
-        
-        app_procesar_mensaje(ptr_app);
-
+        /* Envío el mensaje*/
 		sf_mensaje_procesado_enviar(handler, ptr_app->mensaje );
 	}
 }
@@ -54,71 +53,210 @@ app_t* app_crear(void)
  * @brief Inicializa la estructura app
  * 
  * @param handler_app 
- * @return true 
- * @return false 
  */
-bool app_init(app_t* handler_app)
+void app_init(app_t* handler_app)
 {
-
+    /* Inicio las palabras en cero */
+    for (uint32_t i = 0 ; i < CANT_PALABRAS_MAX ; i++)
+        for (uint32_t j = 0 ; j < CANT_LETRAS_MAX ; j++)
+        {
+            handler_app->palabras[i][j] = 0;
+        }
+    handler_app->error_type = SIN_ERROR;
 }
 
 /**
- * @brief Extrae las palabras del mensaje y las guarda en su propia estructura
+ * @brief Extrae las palabras del mensaje y las guarda en la estructura app_t
  * 
  *          También verifica que los caracteres del mensaje sean correctos llamando
  *          a la función app_validar_mensaje(app_t* handler_app , tMensaje *mensaje);
  * 
  * @param handler_app 
+ * @return true     Si pudo extraer con éxito todas las palabras
+ * @return false    Si hubo algún error, el error queda registrado dentro de la estructura app_t
  */
-static void app_extraer_palabras(app_t* handler_app)
+  
+static bool app_extraer_palabras(app_t* handler_app)
 {
+    uint32_t palabra = PALABRA_INICIAL;
+    uint32_t caracter = CARACTER_INICIAL;
+    
+    /* Si el caracter final es guion bajo o espacio salgo con error*/       // R_C3_9
+    if ( (handler_app->mensaje.ptr_datos[handler_app->mensaje.cantidad -1] == ' ') || (handler_app->mensaje.ptr_datos[handler_app->mensaje.cantidad -1] == '_') )
+        {
+            handler_app->error_type = ERROR_INVALID_DATA;
+            return false;
+        }
 
-}
+    /* recorro todo el mensaje para extrer las palabras y verifico que los caracteres sean válidos.*/ 
+    for ( uint32_t i = INDICE_CAMPO_DATOS; i < handler_app->mensaje.cantidad ; i++)
+    {
+        
+        /* Si el caracter está en mayuscula lo paso a minúscula */
+        if ( ('A' <= handler_app->mensaje.ptr_datos[i]) && ( handler_app->mensaje.ptr_datos[i]<= 'Z'))     // R_C3_7
+            handler_app->mensaje.ptr_datos[i] += A_MINUSCULA;
+        
+        /* Copio el caracter al array de palabras*/ 
+        if ( ('a' <= handler_app->mensaje.ptr_datos[i]) && (handler_app->mensaje.ptr_datos[i] <= 'z'))     // R_C3_7
+        {
+            handler_app->palabras[palabra][caracter] = handler_app->mensaje.ptr_datos[i];
+            caracter++;    
+        }
+        /* Si el carcter era guion bajo o espacio cambio de palabra*/ 
+        else if ( (handler_app->mensaje.ptr_datos[i] == '_' ) || (handler_app->mensaje.ptr_datos[i] == ' ' ) )
+        {
+            /* Si hay 2 guiones bajos o espacios seguidos salgo con error*/             // R_C3_8
+            if (handler_app->mensaje.ptr_datos[i] == handler_app->mensaje.ptr_datos[i+1])
+                {
+                    handler_app->error_type = ERROR_INVALID_DATA;
+                    return false;
+                }            
+            
+            palabra++;                   // incrementeo para cambiar de palabra
+            caracter = CARACTER_INICIAL; // cambio al caracter inicial de la palabra.
+        }
+        /* Si no era un carcter, o guion bajo o espacio, marco el error y salgo*/
+        else
+        {
+            handler_app->error_type = ERROR_INVALID_DATA;
+            return false;
+        }
 
-/**
- * @brief Verifica que los caracteres sean letras, espacio o guion bajo "_"
- * 
- * @param handler_app 
- * @return true 
- * @return false 
- */
-static bool app_validar_mensaje(app_t* handler_app)
-{
-
+    }
+    return true;
 }
 
 /**
  * @brief Tomas las palabras extraidas en la estructura y según el campo C del mensaje
- *          aplica el formato correspondiente y lo guarda en
+ *          aplica el formato correspondiente guardando el mensaje dentro de la estructura app_t
  * 
  * @param handler_app 
+ * @return true     Si el OPCODE era correcto
+ * @return false    Si hubo error en el OPCODE
  */
-static void app_procesar_mensaje(app_t* handler_app)
+static bool app_procesar_mensaje(app_t* handler_app)
 {
-    switch(handler_app->mensaje.ptr_datos[0])
+    switch(handler_app->mensaje.ptr_datos[0])           // R_C3_12
     {
-        case 'C':
+        case 'C':                                       // A camelCase
         {
-            
-            break;
-        }
-        case 'P':
-        {
+            handler_app->mensaje.cantidad = INDICE_CAMPO_DATOS; // Acá iniciará el campo de datos.
 
-            break;
+            /* Bucle para recorrer todas las palabras*/ 
+            for (uint32_t i = 0 ; i < CANT_PALABRAS_MAX ; i++)
+            {
+                if (handler_app->palabras[i][CARACTER_INICIAL] == 0 ) 
+                    break;  // Si el primer caracter de la palabra es cero, significa que termine con todas las palabras y salgo del for
+                for (uint32_t j = 0 ; j < CANT_LETRAS_MAX ; j++)
+                {
+                    if (handler_app->palabras[i][j] == 0 ) 
+                        break; //Si es cero, llegué al final de la palabra y salgo del for.
+                    
+                    /* A partir de la segunda palabra, pongo la primer letra de la palabra en mayúscula */
+                    if ( (i > 0) && (j == 0))
+                        handler_app->palabras[i][j] += A_MAYUSCULA;
+                    
+                    /* Cargo en el mensaje el caracter correspondiente*/ 
+                    handler_app->mensaje.ptr_datos[handler_app->mensaje.cantidad] = handler_app->palabras[i][j];
+                    /* Una vez cargado el caracter, lo reinicio para que el array palabras sea reutilizado*/
+                    handler_app->palabras[i][j] = 0;
+                    /* Incremento el tamaño del paquete*/
+                    handler_app->mensaje.cantidad++;
+                }
+            }
+            break; // Para salir del case.
         }
-        case 'S':
+        case 'P':                                       // A PascalCase
         {
+            handler_app->mensaje.cantidad = INDICE_CAMPO_DATOS; // Acá iniciará el campo de datos.
 
-            break;
+            /* Bucle para recorrer todas las palabras*/ 
+            for (uint32_t i = 0 ; i < CANT_PALABRAS_MAX ; i++)
+            {
+                if (handler_app->palabras[i][CARACTER_INICIAL] == 0 ) 
+                    break;  // Si el primer caracter de la palabra es cero, significa que termine con todas las palabras y salgo del for
+                for (uint32_t j = 0 ; j < CANT_LETRAS_MAX ; j++)
+                {
+                    if (handler_app->palabras[i][j] == 0 ) 
+                        break; //Si es cero, llegué al final de la palabra y salgo del for.
+                    
+                    /* Pongo la primer letra de la palabra en mayúscula */
+                    if ( j == 0 )
+                        handler_app->palabras[i][j] += A_MAYUSCULA;
+                    
+                    /* Cargo en el mensaje el caracter correspondiente*/ 
+                    handler_app->mensaje.ptr_datos[handler_app->mensaje.cantidad] = handler_app->palabras[i][j];
+                    /* Una vez cargado el caracter, lo reinicio para que el array palabras sea reutilizado*/
+                    handler_app->palabras[i][j] = 0;
+                    /* Incremento el tamaño del paquete*/
+                    handler_app->mensaje.cantidad++;
+                }
+            }
+            break; // Para salir del case.
         }
-        default:
+        case 'S':                                       // A snake_case
         {
+            handler_app->mensaje.cantidad = INDICE_CAMPO_DATOS; // Acá iniciará el campo de datos.
 
-            break;
+            /* Bucle para recorrer todas las palabras*/ 
+            for (uint32_t i = 0 ; i < CANT_PALABRAS_MAX ; i++)
+            {
+                if (handler_app->palabras[i][CARACTER_INICIAL] == 0 ) 
+                    break;  // Si el primer caracter de la palabra es cero, significa que termine con todas las palabras y salgo del for
+                for (uint32_t j = 0 ; j < CANT_LETRAS_MAX ; j++)
+                {
+                    if (handler_app->palabras[i][j] == 0 ) 
+                        break; //Si es cero, llegué al final de la palabra y salgo del for.
+                    
+                    /* A partir de la segunda palabra, inserto un '_' al inicio */
+                    if ( (i > 0) && (j == 0) )
+                        {
+                            handler_app->mensaje.ptr_datos[handler_app->mensaje.cantidad] = '_';
+                            handler_app->mensaje.cantidad++;
+                        }
+                     
+                    /* Cargo en el mensaje el caracter correspondiente*/ 
+                    handler_app->mensaje.ptr_datos[handler_app->mensaje.cantidad] = handler_app->palabras[i][j];
+                    /* Una vez cargado el caracter, lo reinicio para que el array palabras sea reutilizado*/
+                    handler_app->palabras[i][j] = 0;
+                    /* Incremento el tamaño del paquete*/
+                    handler_app->mensaje.cantidad++;
+                }
+            }
+            break; // Para salir del case.
+        }
+        default:                                                // R_C3_6 - R_C3_11
+        {
+            handler_app->error_type = ERROR_INVALID_OPCODE;
+            return false;            
         }
         
     }
-    
+    return true;
+}
 
+/**
+ * @brief Inserta el mensaje de error dentro del campo de mensaje de app_t
+ * 
+ * @param handler_app 
+ */
+static void app_insertar_mensaje_error(app_t* handler_app)      // R_C3_13
+{
+    handler_app->mensaje.ptr_datos[0] = 'E';    
+    switch ( handler_app->error_type )
+    {
+    case ERROR_INVALID_DATA:
+        handler_app->mensaje.ptr_datos[1] = '0'; 
+        handler_app->mensaje.ptr_datos[2] = '0';
+        handler_app->mensaje.cantidad = 3;
+        break;
+    
+    case ERROR_INVALID_OPCODE:
+        handler_app->mensaje.ptr_datos[1] = '0'; 
+        handler_app->mensaje.ptr_datos[2] = '1';
+        handler_app->mensaje.cantidad = 3;
+        break;
+    default:
+        break;
+    }
 }
